48382b563bd6ec086ff2e5f28a2f227a3207ec31
Nuloi <ekgus8056@gmail.com>
2023-03-10 19:06:17 +0900

    0310

--- DFS/24479.py
+++ DFS/24479.py
@@ -1,25 +1,28 @@
-n,m,l=map(int,input().split())
+import sys
 
-graph = [[] for _ in range(n)]
-visited = [False] * (n)
-num=[0]*n
+n, m, l = map(int, input().split())
 
-for i in range(m):
+visited = set()
+num = []
+graph = [[] for _ in range(n+1)]
+
+for j in range(m):
     a, b = map(int, input().split())
-    graph[a-1].append(b-1)
-    graph[b-1].append(a-1)
-for i in range(n):
-    graph[i].sort() # 인접 정점을 오름차순으로 정렬
+    graph[a].append(b)
+    graph[b].append(a)
 
-def dfs(graph, v, visited):
-    visited[v] = True
-    num[v] = 1
+def dfs(v):
     for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
-    return False
+        if i not in visited:
+            visited.add(i)
+            num.append(i)
+            dfs(i)
 
-dfs(graph,l-1,visited,num)
+visited.add(l)
+num.append(l)
+dfs(l)
+
+num.extend([0]*(n-len(num)))
 
 for i in range(n):
-    print(num[i]*i)
+    print(num[i])
